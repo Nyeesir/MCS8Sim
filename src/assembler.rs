58ea82b1e2f7ea841d4e18f4c78226054a69e1a3
Nyees <128thbit@gmail.com>
2025-08-05 17:50:48 +0200

    Working on assembler.rs

--- src/assembler.rs
+++ src/assembler.rs
@@ -1,31 +1,62 @@
-pub fn assemble (data: &str){
-    const MEMORY_SIZE: usize = 65536;
+use std::{error::Error, fmt};
+
+const MEMORY_SIZE: usize = 65536;
+
+#[derive(Debug, Clone)]
+pub struct AssemblyError{
+    line_number: usize,
+    message: String
+}
+
+impl Error for AssemblyError {}
+impl fmt::Display for AssemblyError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Error in line {}: {}", self.line_number, self.message)
+    }
+}
+
+pub fn assemble (data: &str) -> Result<[u8; MEMORY_SIZE], AssemblyError> {
     let _memory: [u8; MEMORY_SIZE] = [0; MEMORY_SIZE];
-    let _pointer: u16 = 0;
+    let _pointer: usize = 0;
+    let mut line_number: usize = 0;
 
     let lines = data.lines();
     for line in lines{
+        line_number += 1;
         if line.is_empty() {continue}
 
         let mut tokens_iter = line.trim().split_whitespace();
 
-        let token = tokens_iter.next().expect("Non-empty line doesn't contain a word somehow");
+        let token_option = tokens_iter.next();
+        let token = match token_option {
+            Some(x) => x,
+            None => return Err(AssemblyError{ line_number, message:"Non-empty line doesn't contain a word somehow".into()})
+        };
+
         if token.contains(":") {
             handle_label(token);
             let token = tokens_iter.next();
             match token {
-                Some(token) => encode_instruction(token),
+                Some(token) => translate_instruction(token),
                 None => (continue)
             }
         }
         else {
-            encode_instruction(token)
+            translate_instruction(token)
         }
     }
+
+    Ok(_memory)
 }
 
-fn encode_instruction(instruction: &str){
+fn translate_instruction(instruction: &str){
+    let opcode : u8;
+    let operands : u8;
     match instruction {
+        "NOP" => {
+            opcode = 0x00;
+            operands = 0;
+        }
         "ADI" => {
 
         }
