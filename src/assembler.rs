6bc4dae3dff643d23566192118789d462217b97f
Nyees <128thbit@gmail.com>
2025-08-05 17:50:48 +0200

    Refactoring

--- src/assembler.rs
+++ src/assembler.rs
@@ -1,36 +1,67 @@
-pub fn assemble (data: &str){
-    const MEMORY_SIZE: usize = 65536;
-    let _memory: [u8; MEMORY_SIZE] = [0; MEMORY_SIZE];
-    let _pointer: u16 = 0;
+use std::{error::Error, fmt};
 
-    let lines = data.lines();
-    for line in lines{
-        if line.is_empty() {continue}
+const MEMORY_SIZE: usize = 65536;
 
-        let mut tokens_iter = line.trim().split_whitespace();
+#[derive(Debug, Clone)]
+pub struct AssemblyError{
+    line_number: usize,
+    message: String
+}
 
-        let token = tokens_iter.next().expect("Non-empty line doesn't contain a word somehow");
-        if token.contains(":") {
-            handle_label(token);
-            let token = tokens_iter.next();
-            match token {
-                Some(token) => encode_instruction(token),
-                None => (continue)
-            }
-        }
-        else {
-            encode_instruction(token)
-        }
+impl Error for AssemblyError {}
+impl fmt::Display for AssemblyError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Error in line {}: {}", self.line_number, self.message)
     }
 }
 
-fn encode_instruction(instruction: &str){
-    match instruction {
-        "ADI" => {
+pub fn assemble (data: &str) -> Result<[u8; MEMORY_SIZE], AssemblyError> {
+    let _memory: [u8; MEMORY_SIZE] = [0; MEMORY_SIZE];
+    let _pointer: usize = 0;
+    let mut line_number: usize = 0;
 
+    let lines = data.lines();
+    for line in lines{
+        line_number += 1;
+        if line.is_empty() {continue}
+
+        let line = line.to_uppercase();
+        let mut tokens_iter = line.trim().split_whitespace();
+
+        let token = match (tokens_iter.next()) {
+            Some(x) => x,
+            None => return Err(AssemblyError{ line_number, message:"Non-empty line doesn't contain a word somehow".into()})
+        };
+
+        let instruction: &str;
+        if token.contains(":") {
+            handle_label(token);
+            instruction = match(tokens_iter.next()) {
+                Some(x) => x,
+                None => continue
+            };
         }
-        _ => println!("unknown command")
+        else {
+            instruction = token;
+        }
+        let operand = (tokens_iter.next()).unwrap_or_else(|| "");
+        translate_instruction(instruction, operand);
     }
+
+    Ok(_memory)
+}
+
+fn translate_instruction(instruction: &str, operand: &str){
+    let opcode : u8;
+    match instruction {
+        "NOP" => opcode = 0x00,
+        "MOV" => match operand {
+            "B,B" => opcode = 0x00,
+            _ => opcode = 0x00
+        }
+        _ => opcode = 0x00
+    }
+
 }
 
 fn handle_label(label: &str){
