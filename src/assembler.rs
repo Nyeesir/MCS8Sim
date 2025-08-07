30ab0d74f19ac3d80da7a377425c622838a055af
Nyees <128thbit@gmail.com>
2025-08-07 16:30:03 +0200

    Refactoring of assembler.rs, continuing development

--- src/assembler.rs
+++ src/assembler.rs
@@ -27,83 +27,110 @@ impl fmt::Display for InvaildTokenError {
     }
 }
 
-enum OperandType{
-    Register,
-    RegisterPair,
-    Immediate,
-    Address,
+#[derive(Debug, Clone)]
+pub struct DuplicateLabelError{
+    label: String
 }
 
-// struct AssemblerData{
-//     memory: [u8; MEMORY_SIZE],
-//     memory_pointer: usize,
-//     jump_map: HashMap<String, usize>
-// }
+impl Error for DuplicateLabelError {}
+impl fmt::Display for DuplicateLabelError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Duplicate label: {}", self.label)
+    }
+}
 
-pub fn assemble (data: &str) -> Result<[u8; MEMORY_SIZE], AssemblyError> {
-    let _memory: [u8; MEMORY_SIZE] = [0; MEMORY_SIZE];
-    let _pointer: usize = 0;
-    let mut jump_map = HashMap::new();
-    let mut line_number: usize = 0;
+pub struct Assembler{
+    memory: [u8; MEMORY_SIZE],
+    memory_pointer: usize,
+    jump_map: HashMap<String, usize>
+}
 
-    let lines = data.lines();
-    for line in lines{
-        line_number += 1;
-        let line = line.trim().to_uppercase();
-        if line.is_empty() {continue}
+impl Assembler{
+    pub fn assemble (&mut self, data: &str) -> Result<[u8; MEMORY_SIZE], AssemblyError> {
+        let mut line_number: usize = 0;
 
-        let mut tokens_iter = line.split_whitespace();
+        let lines = data.lines();
+        for line in lines{
+            line_number += 1;
+            let line = line.trim().to_uppercase();
+            if line.is_empty() {continue}
 
-        let token = match (tokens_iter.next()) {
-            Some(x) => x,
-            None => return Err(AssemblyError{ line_number, message:"Non-empty line doesn't contain a word somehow".into()})
-        };
+            let mut tokens_iter = line.split_whitespace();
 
-        let instruction: &str;
-        if token.contains(":") {
-            let token = token.replace(":","");
-            jump_map.insert(token, line_number);
-            instruction = match(tokens_iter.next()) {
+            let token = match (tokens_iter.next()) {
                 Some(x) => x,
-                None => continue
+                None => return Err(AssemblyError{ line_number, message:"Non-empty line doesn't contain a word somehow".into()})
+            };
+
+            let instruction: &str;
+            if token.contains(":") {
+                match (Self::add_jump_point(self,token)) {
+                    Ok(_) => {},
+                    Err(e) => return Err(AssemblyError{ line_number, message:e.to_string()})
+                }
+                instruction = match(tokens_iter.next()) {
+                    Some(x) => x,
+                    None => continue
+                };
+            }
+            else {
+                instruction = token;
+            }
+            let operand = (tokens_iter.next()).unwrap_or_else(|| "");
+            let opcodes = match (Self::translate_instruction(instruction, operand)){
+                Ok(x) => x,
+                Err(e) => return Err(AssemblyError{ line_number, message:e.to_string()})
             };
         }
-        else {
-            instruction = token;
-        }
-        let operand = (tokens_iter.next()).unwrap_or_else(|| "");
-        let opcodes = match (translate_instruction(instruction, operand)){
-            Ok(x) => x,
-            Err(e) => return Err(AssemblyError{ line_number, message:e.to_string()})
-        };
+
+        Ok(self.memory)
     }
 
-    Ok(_memory)
-}
-
-fn translate_instruction(instruction: &str, operands: &str) -> Result<u8, InvaildTokenError>{
-    let opcode : u8;
-    match instruction {
-        "NOP" => opcode = 0x00,
-        _ => return Err(InvaildTokenError{ token: instruction.into()})
+    fn translate_instruction(instruction: &str, operands: &str) -> Result<[u8;3], InvaildTokenError>{
+        let mut opcode : u8;
+        match instruction {
+            "NOP" => opcode = 0b00,
+            "MOV" => {
+                opcode = 0b01000000;
+                let left_register = Self::translate_register(operands)?;
+                let right_register = Self::translate_register(operands)?;
+                opcode &= (left_register << 3) & right_register;
+            }
+            _ => return Err(InvaildTokenError{ token: instruction.into()})
+        }
+        Ok([opcode,0,0])
     }
-    Ok((0x00))
-}
 
-fn translate_operand(operands: &str, operand_type: OperandType) -> Result<[u8;2], InvaildTokenError>{
-    match operand_type {
-        OperandType::Register => {
-
-        }
-        OperandType::RegisterPair => {
-
-        }
-        OperandType::Immediate => {
-
-        }
-        OperandType::Address => {
-
+    fn translate_register(register: &str) -> Result<u8, InvaildTokenError>{
+        match register {
+            "B" => Ok(0b000),
+            "C" => Ok(0b001),
+            "D" => Ok(0b010),
+            "E" => Ok(0b011),
+            "H" => Ok(0b100),
+            "L" => Ok(0b101),
+            "M" => Ok(0b110),
+            "A" => Ok(0b111),
+            _ => Err(InvaildTokenError{ token: register.into()})
         }
     }
-    Ok([0,0])
+
+    fn translate_register_pair(register_pair: &str) -> Result<u8, InvaildTokenError>{
+        match register_pair {
+            "BC" | "B" => Ok(0b00),
+            "DE" | "D" => Ok(0b01),
+            "HL" | "H" => Ok(0b11),
+            "SP" | "PSW" => Ok(0b11),
+            _ => Err(InvaildTokenError{ token: register_pair.into()})
+        }
+    }
+
+    fn add_jump_point(&mut self, label: &str) -> Result<(), DuplicateLabelError> {
+        if self.jump_map.contains_key(label){
+            return Err(DuplicateLabelError{ label: label.into()})
+        }
+
+        self.jump_map.insert(label.into(), self.memory_pointer);
+        Ok(())
+    }
 }
--- src/tests.rs
+++ src/tests.rs
@@ -1,11 +1,7 @@
 use super::*;
 #[test]
 fn assembler_test_1() {
-    let data = "ADI 30H \n\r
-                        RST 1 \n\r
-                        DCX H \n\r
-                        DCR B \n\r" ;
-    assembler::assemble(data);
+    let data = "MOV B,B";
     assert!(false);
 }
 
