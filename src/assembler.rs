b2cbc2e421360a4bfdf4b83108a447bc68259e0d
Nyees <128thbit@gmail.com>
2025-08-29 15:45:54 +0200

    Extending error messages and starting work on parsing numeric and ascii operands

--- src/assembler.rs
+++ src/assembler.rs
@@ -2,28 +2,49 @@ use std::{error::Error, fmt, collections::HashMap};
 
 const MEMORY_SIZE: usize = 65536;
 
+#[derive(Clone, Debug)]
+enum TokenType{
+    Instruction,
+    Operand,
+    Label,
+    Address,
+}
 #[derive(Debug, Clone)]
 pub struct AssemblyError{
     line_number: usize,
+    line_text: String,
     message: String
 }
 
 impl Error for AssemblyError {}
 impl fmt::Display for AssemblyError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Error in line {}: {}", self.line_number, self.message)
+        write!(f, "Error in line {} - {}:\n{}", self.line_number, self.line_text, self.message)
     }
 }
 
 #[derive(Debug, Clone)]
 pub struct InvaildTokenError{
-    token: String
+    token: String,
+    token_type: TokenType,
+    additional_info: Option<String>
 }
 
 impl Error for InvaildTokenError {}
 impl fmt::Display for InvaildTokenError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Invaild token: {}", self.token)
+        let mut error_message: String;
+        match self.token_type {
+            TokenType::Instruction => error_message = "Invalid instruction".into(),
+            TokenType::Operand => error_message = "Invalid operand".into(),
+            TokenType::Label => error_message = "Invalid label".into(),
+            TokenType::Address => error_message = "Invalid address".into(),
+        }
+        error_message.push_str(&format!(": {}.", self.token));
+        if let Some(x) = &self.additional_info {
+            error_message.push_str(&format!("Additional info: {}", x));
+        }
+        write!(f, "{}", error_message)
     }
 }
 
@@ -67,14 +88,23 @@ impl Assembler{
 
             let token = match tokens_iter.next() {
                 Some(x) => x,
-                None => return Err(AssemblyError{ line_number, message:"Non-empty line doesn't contain a word somehow".into()})
+                None => return Err(AssemblyError{ line_number, line_text: line, message:"Non-empty line doesn't contain a word somehow".into()})
             };
 
             let instruction: &str;
-            if token.contains(":") {
+            if token.ends_with(":") {
+                // TODO: walidacja nazw labeli
+                /*
+                    Here are some invalid label fields:
+                        123: begins with a decimal digit
+                        LABEL is not followed by a colon
+                        ADD: is an operation code
+                        END: is a pseudo-instruction
+                 */
+
                 match Self::add_jump_point(self, token) {
                     Ok(_) => {},
-                    Err(e) => return Err(AssemblyError{ line_number, message:e.to_string()})
+                    Err(e) => return Err(AssemblyError{ line_number, line_text:line, message:e.to_string()})
                 }
                 instruction = match tokens_iter.next() {
                     Some(x) => x,
@@ -87,14 +117,14 @@ impl Assembler{
             let operand = tokens_iter.next().unwrap_or_else(|| "");
             let opcodes = match Self::translate_instruction(instruction, operand) {
                 Ok(x) => x,
-                Err(e) => return Err(AssemblyError{ line_number, message:e.to_string()})
+                Err(e) => return Err(AssemblyError{ line_number, line_text:line, message:e.to_string()})
             };
 
             for opcode in opcodes.iter(){
                 self.memory[self.memory_pointer] = opcode.to_owned();
                 self.memory_pointer += 1;
                 if self.memory_pointer >= MEMORY_SIZE {
-                    return Err(AssemblyError{ line_number, message: "Memory overflow".into()})
+                    return Err(AssemblyError{ line_number, line_text:line, message: "Memory overflow".into()})
                 }
             }
         }
@@ -123,7 +153,7 @@ impl Assembler{
             "MOV" => {
                 let (left_operand, right_operand) = match operands.split_once(","){
                     Some(x) => x,
-                    None => return Err(InvaildTokenError{ token: operands.into()})
+                    None => return Err(InvaildTokenError{ token: operands.into(), token_type: TokenType::Operand, additional_info: None })
                 };
                 opcodes.push(0b01000000);
                 let left_register = Self::translate_register(left_operand)?;
@@ -133,7 +163,7 @@ impl Assembler{
             "STAX" => {
                 match operands {
                     "BC" | "B" | "DE" | "D" => {}
-                    _ => return Err(InvaildTokenError{ token: operands.into()})
+                    _ => return Err(InvaildTokenError{ token: operands.into(), token_type: TokenType::Operand, additional_info: Some("Only BC, B, DE, D are allowed".into())})
                 }
                 opcodes.push(0b00000010);
                 let register_pair = Self::translate_register_pair(operands)?;
@@ -142,7 +172,7 @@ impl Assembler{
             "LDAX" => {
                 match operands {
                     "BC" | "B" | "DE" | "D" => {}
-                    _ => return Err(InvaildTokenError{ token: operands.into()})
+                    _ => return Err(InvaildTokenError{ token: operands.into(), token_type: TokenType::Operand, additional_info: Some("Only BC, B, DE, D are allowed".into())})
                 }
                 opcodes.push(0b00001010);
                 let register_pair = Self::translate_register_pair(operands)?;
@@ -222,9 +252,17 @@ impl Assembler{
             "XTHL" => opcodes.push(0b11100011),
             "SPHL" => opcodes.push(0b11111001),
             "MVI" => {
-
+                opcodes.push(0b00000110);
+                let (left_operand, right_operand) = match operands.split_once(","){
+                    Some(x) => x,
+                    None => return Err(InvaildTokenError{ token: operands.into(), token_type: TokenType::Operand, additional_info: None})
+                };
+                let register = Self::translate_register(left_operand)?;
+                opcodes[0] |= register << 3;
+                let value = Self::translate_value(right_operand)?;
+                opcodes.push(value);
             }
-            _ => return Err(InvaildTokenError{ token: instruction.into()})
+            _ => return Err(InvaildTokenError{ token: instruction.into(), token_type: TokenType::Instruction, additional_info: None})
         }
         Ok(opcodes)
     }
@@ -239,7 +277,7 @@ impl Assembler{
             "L" => Ok(0b101),
             "M" => Ok(0b110),
             "A" => Ok(0b111),
-            _ => Err(InvaildTokenError{ token: register.into()})
+            _ => Err(InvaildTokenError{ token: register.into(), token_type: TokenType::Operand, additional_info: Some("Only registers are allowed".into())})
         }
     }
 
@@ -249,7 +287,7 @@ impl Assembler{
             "DE" | "D" => Ok(0b01),
             "HL" | "H" => Ok(0b10),
             "SP" | "PSW" => Ok(0b11),
-            _ => Err(InvaildTokenError{ token: register_pair.into()})
+            _ => Err(InvaildTokenError{ token: register_pair.into(), token_type: TokenType::Operand, additional_info: Some("Only register pairs are allowed".into())})
         }
     }
 
@@ -257,7 +295,18 @@ impl Assembler{
         Ok(0)
     }
 
-    fn _translate_number(_number: &str) -> Result<u8, InvaildTokenError>{
+    fn translate_value(value: &str) -> Result<u8, InvaildTokenError>{
+        //sprawdzić czy 'x' w ascii, potem normalne liczby a potem liczby z dopiskami wskazujacymi format liczby
+        if value.len() == 3 && value.starts_with("\'") && value.ends_with("\'") {
+            //puste ascii??
+            let chars = value.chars().collect::<Vec<char>>();
+            let ret: char = chars[1];
+            if ret.is_ascii(){
+                return Ok(ret as u8);
+            }
+        }
+        //TODO: DOK
+        if let Ok(x) = value.parse::<u8>(){}
         Ok(0)
     }
 
--- src/tests.rs
+++ src/tests.rs
@@ -1,7 +1,7 @@
 use super::*;
 #[test]
 fn assembler_test_1() {
-    let data = "MOV B,B \n    add a \n ADD b";
+    let data = "MOV B,B \n    add a \n ADD z";
     let memory = assembler::Assembler::new().assemble(data).unwrap();
 
     assert_eq!(&memory[0..3], &[64,135,128]);
